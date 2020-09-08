697cab571a6cf67f14a3e48606c793c0fba12d63
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-08 17:20:31 -0500

    added event listener

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -2,7 +2,10 @@
 
 (function(){
 
-console.log("G2G")
+    const listener = function (event) {
+        alert("you clicked a button");
+    }
 
+    document.getElementById('1').addEventListener('click', listener);
 
 })();
