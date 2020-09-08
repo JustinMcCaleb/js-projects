dfb23c48f7a223e2a4f199224f977c62d6888af6
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-08 17:34:12 -0500

    added clear event listener

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -6,7 +6,16 @@
         document.getElementById('left-input').setAttribute('value', 'test');
     }
 
+
+    const clear = function () {
+        document.getElementById('left-input').setAttribute('value', '');
+        document.getElementById('middle-input').setAttribute('value', '');
+        document.getElementById('right-input').setAttribute('value', '');
+    }
+
     document.getElementById('1').addEventListener('click', listener);
     document.getElementById('-').addEventListener('click', listener);
+
+    document.getElementById('C').addEventListener()
 
 })();
