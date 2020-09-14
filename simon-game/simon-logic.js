dfb32c71ad6385bd5d9874dbf133a97e8c755135
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-14 16:38:22 -0500

    grabbed color boxes in js with document getElementById

--- simon-game/simon-logic.js
+++ simon-game/simon-logic.js
@@ -7,8 +7,12 @@
 
 
     //adding event listener on start button to start game
-    const startButton = document.getElementById("start-button");
+    const startButton = document.getElementById('start-button');
     startButton.addEventListener('click', startGame);
 
+    const upperLeftBox = document.getElementById('ul');
+    const upperRightBox = document.getElementById('ur');
+    const lowerLeftBox = document.getElementById('ll');
+    const lowerRightBox = document.getElementById('lr');
 
 })();
