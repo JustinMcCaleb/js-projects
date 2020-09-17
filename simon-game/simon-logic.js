f78b82defd64fe517344097387e2e10c42aad226
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-17 11:52:13 -0500

    adding methods to game class

--- simon-game/simon-logic.js
+++ simon-game/simon-logic.js
@@ -10,6 +10,121 @@ $(document).ready(function(){
             this.playerRound = [];
         }
 
+        newGame() {
+            this.count = 0;
+            this.currentGame = [];
+            this.playerRound = [];
+        }
+
+        boxHighlightPattern() {
+            let interval = setInterval(() => {
+                if(count >= pattern.length){
+                    clearInterval(interval)
+                    console.log("all done");
+                }else{
+                    switch (pattern[count]){
+                        case 'ul':
+                            $('#ul').addClass('ul-active');
+                            setTimeout(() => {
+                                $('#ul').removeClass('ul-active');
+                            }, 1000)
+                            break;
+                        case 'ur':
+                            $('#ur').addClass('ur-active');
+                            setTimeout(() => {
+                                $('#ur').removeClass('ur-active');
+                            }, 1000)
+                            break;
+                        case 'll':
+                            $('#ll').addClass('ll-active')
+                            setTimeout(() => {
+                                $('#ll').removeClass('ll-active')
+                            }, 1000)
+                            break;
+                        case 'lr':
+                            $('#lr').addClass('lr-active')
+                            setTimeout(() => {
+                                $('#lr').removeClass('lr-active')
+                            }, 1000)
+                            break;
+                    }
+                    count++
+                }
+            }, 2000)
+        }
+
+
+        newRound() {
+            let scoreboardCounter = document.getElementsByClassName('scoreboard-text');
+            let round = 0;
+            round++;
+            scoreboardCounter.innerText = 'Round: ' + round;
+            this.playerRound = [];
+            setTimeout(() => {
+
+                let randomNumber = getRandomArbitrary(1,4)
+
+                let boxes = ['ul', 'ur', 'll', 'lr'];
+
+                console.log("RANDOM INDEX: " + getRandomIndex(boxes, randomNumber))
+
+                switch (getRandomIndex(boxes, randomNumber)){
+                    case 'ul':
+                        pattern.push('ul');
+                        highlightBox();
+                        break;
+                    case 'ur':
+                        pattern.push('ur');
+                        highlightBox();
+                        break;
+                    case 'll':
+                        pattern.push('ll');
+                        highlightBox();
+                        break;
+                    case 'lr':
+                        pattern.push('lr');
+                        highlightBox();
+                        break;
+                }
+                if(playerPattern.length === pattern.length){
+                    if(JSON.stringify(playerPattern) === JSON.stringify(pattern)){
+                        startGame();
+                    }
+                }
+            }, 2000)
+        }
+
+
+        startGame() {
+
+            let game = Object.create(Game);
+
+            newRound();
+            game.playerRound = [];
+            const upperLeftBox = document.getElementById('ul');
+            const upperRightBox = document.getElementById('ur');
+            const lowerLeftBox = document.getElementById('ll');
+            const lowerRightBox = document.getElementById('lr');
+            upperLeftBox.onclick = () => {
+                playerPattern.push('ul');
+                console.log(playerPattern);
+            }
+            upperRightBox.onclick = () => {
+                playerPattern.push('ur');
+                console.log(playerPattern);
+            }
+            lowerLeftBox.onclick = () => {
+                playerPattern.push('ll')
+                console.log(playerPattern);
+            }
+            lowerRightBox.onclick = () => {
+                playerPattern.push('lr');
+                console.log(playerPattern);
+            }
+            console.log("PLAYER ROUND: " + playerPattern.length);
+            console.log("PATTERN LENGTH: " + pattern.length);
+        }
+
     }
 
     const getRandomArbitrary = (min, max) => {
@@ -22,54 +137,55 @@ $(document).ready(function(){
         return array[num];
     }
 
-    Game.prototype.highlightBox = function () {
-        console.log("==========================")
-        console.log(pattern);
-        let count = 0;
 
-        let interval = setInterval(() => {
-            if(count >= pattern.length){
-                clearInterval(interval)
-                console.log("all done");
-            }else{
-                switch (pattern[count]){
-                    case 'ul':
-                            // $('#ul').css('background-color', 'rgba(255,0,144,1)');
-                            $('#ul').addClass('ul-active');
-                            setTimeout(() => {
-                                // $('#ul').css('background-color', 'rgba(255,0,144,.3)');
-                                $('#ul').removeClass('ul-active');
-                            }, 1000)
-                        break;
-                    case 'ur':
-                            // $('#ur').css('background-color', 'rgba(77,77,255,1)');
-                            $('#ur').addClass('ur-active');
-                            setTimeout(() => {
-                                // $('#ur').css('background-color', 'rgba(77,77,255,.3)');
-                                $('#ur').removeClass('ur-active');
-                            }, 1000)
-                        break;
-                    case 'll':
-                            // $('#ll').css('background-color', 'rgba(0,255,0,1)');
-                            $('#ll').addClass('ll-active')
-                            setTimeout(() => {
-                                // $('#ll').css('background-color', 'rgba(0,255,0,.3)');
-                                $('#ll').removeClass('ll-active')
-                            }, 1000)
-                        break;
-                    case 'lr':
-                            // $('#lr').css('background-color', 'rgba(255,255,0,1)');
-                            $('#lr').addClass('lr-active')
-                            setTimeout(() => {
-                                // $('#lr').css('background-color', 'rgba(255,255,0,.3)');
-                                $('#lr').removeClass('lr-active')
-                            }, 1000)
-                        break;
-                }
-                count++
-            }
-        }, 2000)
-    }
+    // Game.prototype.highlightBox = function () {
+    //     console.log("==========================")
+    //     console.log(pattern);
+    //     let count = 0;
+    //
+    //     let interval = setInterval(() => {
+    //         if(count >= pattern.length){
+    //             clearInterval(interval)
+    //             console.log("all done");
+    //         }else{
+    //             switch (pattern[count]){
+    //                 case 'ul':
+    //                         // $('#ul').css('background-color', 'rgba(255,0,144,1)');
+    //                         $('#ul').addClass('ul-active');
+    //                         setTimeout(() => {
+    //                             // $('#ul').css('background-color', 'rgba(255,0,144,.3)');
+    //                             $('#ul').removeClass('ul-active');
+    //                         }, 1000)
+    //                     break;
+    //                 case 'ur':
+    //                         // $('#ur').css('background-color', 'rgba(77,77,255,1)');
+    //                         $('#ur').addClass('ur-active');
+    //                         setTimeout(() => {
+    //                             // $('#ur').css('background-color', 'rgba(77,77,255,.3)');
+    //                             $('#ur').removeClass('ur-active');
+    //                         }, 1000)
+    //                     break;
+    //                 case 'll':
+    //                         // $('#ll').css('background-color', 'rgba(0,255,0,1)');
+    //                         $('#ll').addClass('ll-active')
+    //                         setTimeout(() => {
+    //                             // $('#ll').css('background-color', 'rgba(0,255,0,.3)');
+    //                             $('#ll').removeClass('ll-active')
+    //                         }, 1000)
+    //                     break;
+    //                 case 'lr':
+    //                         // $('#lr').css('background-color', 'rgba(255,255,0,1)');
+    //                         $('#lr').addClass('lr-active')
+    //                         setTimeout(() => {
+    //                             // $('#lr').css('background-color', 'rgba(255,255,0,.3)');
+    //                             $('#lr').removeClass('lr-active')
+    //                         }, 1000)
+    //                     break;
+    //             }
+    //             count++
+    //         }
+    //     }, 2000)
+    // }
 
 
     const userRound = () => {
@@ -81,77 +197,77 @@ $(document).ready(function(){
         //if user click pattern matches pattern then call startGame again
     }
 
-    const newRound = () => {
-        let scoreboardCounter = document.getElementsByClassName('scoreboard-text');
-        let round = 0;
-        round++;
-        scoreboardCounter.innerText = 'Round: ' + round;
-        playerPattern = [];
-        setTimeout(() => {
+    // const newRound = () => {
+    //     let scoreboardCounter = document.getElementsByClassName('scoreboard-text');
+    //     let round = 0;
+    //     round++;
+    //     scoreboardCounter.innerText = 'Round: ' + round;
+    //     playerPattern = [];
+    //     setTimeout(() => {
+    //
+    //         let randomNumber = getRandomArbitrary(1,4)
+    //
+    //         let boxes = ['ul', 'ur', 'll', 'lr'];
+    //
+    //         console.log("RANDOM INDEX: " + getRandomIndex(boxes, randomNumber))
+    //
+    //         switch (getRandomIndex(boxes, randomNumber)){
+    //             case 'ul':
+    //                 pattern.push('ul');
+    //                 highlightBox();
+    //                 break;
+    //             case 'ur':
+    //                 pattern.push('ur');
+    //                 highlightBox();
+    //                 break;
+    //             case 'll':
+    //                 pattern.push('ll');
+    //                 highlightBox();
+    //                 break;
+    //             case 'lr':
+    //                 pattern.push('lr');
+    //                 highlightBox();
+    //                 break;
+    //             }
+    //         if(playerPattern.length === pattern.length){
+    //             if(JSON.stringify(playerPattern) === JSON.stringify(pattern)){
+    //                 startGame();
+    //             }
+    //         }
+    //     }, 2000)
+    // }
 
-            let randomNumber = getRandomArbitrary(1,4)
-
-            let boxes = ['ul', 'ur', 'll', 'lr'];
-
-            console.log("RANDOM INDEX: " + getRandomIndex(boxes, randomNumber))
-
-            switch (getRandomIndex(boxes, randomNumber)){
-                case 'ul':
-                    pattern.push('ul');
-                    highlightBox();
-                    break;
-                case 'ur':
-                    pattern.push('ur');
-                    highlightBox();
-                    break;
-                case 'll':
-                    pattern.push('ll');
-                    highlightBox();
-                    break;
-                case 'lr':
-                    pattern.push('lr');
-                    highlightBox();
-                    break;
-                }
-            if(playerPattern.length === pattern.length){
-                if(JSON.stringify(playerPattern) === JSON.stringify(pattern)){
-                    startGame();
-                }
-            }
-        }, 2000)
-    }
 
 
-
-    const startGame = () => {
-
-        let game = Object.create(Game);
-
-        newRound();
-        game.playerRound = [];
-        const upperLeftBox = document.getElementById('ul');
-        const upperRightBox = document.getElementById('ur');
-        const lowerLeftBox = document.getElementById('ll');
-        const lowerRightBox = document.getElementById('lr');
-        upperLeftBox.onclick = () => {
-            playerPattern.push('ul');
-            console.log(playerPattern);
-        }
-        upperRightBox.onclick = () => {
-            playerPattern.push('ur');
-            console.log(playerPattern);
-        }
-        lowerLeftBox.onclick = () => {
-            playerPattern.push('ll')
-            console.log(playerPattern);
-        }
-        lowerRightBox.onclick = () => {
-            playerPattern.push('lr');
-            console.log(playerPattern);
-        }
-        console.log("PLAYER ROUND: " + playerPattern.length);
-        console.log("PATTERN LENGTH: " + pattern.length);
-    }
+    // const startGame = () => {
+    //
+    //     let game = Object.create(Game);
+    //
+    //     newRound();
+    //     game.playerRound = [];
+    //     const upperLeftBox = document.getElementById('ul');
+    //     const upperRightBox = document.getElementById('ur');
+    //     const lowerLeftBox = document.getElementById('ll');
+    //     const lowerRightBox = document.getElementById('lr');
+    //     upperLeftBox.onclick = () => {
+    //         playerPattern.push('ul');
+    //         console.log(playerPattern);
+    //     }
+    //     upperRightBox.onclick = () => {
+    //         playerPattern.push('ur');
+    //         console.log(playerPattern);
+    //     }
+    //     lowerLeftBox.onclick = () => {
+    //         playerPattern.push('ll')
+    //         console.log(playerPattern);
+    //     }
+    //     lowerRightBox.onclick = () => {
+    //         playerPattern.push('lr');
+    //         console.log(playerPattern);
+    //     }
+    //     console.log("PLAYER ROUND: " + playerPattern.length);
+    //     console.log("PATTERN LENGTH: " + pattern.length);
+    // }
 
     const clicked = (box) => {
         console.log(box);
