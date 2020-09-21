da01fbc8552620bf60b593cc0904438f52bf8cbf
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-21 16:23:34 -0500

    arrow functions vs old

--- nextGenJS/es6.js
+++ nextGenJS/es6.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//~~~~~~~~~ LET and CONST ~~~~~~~~
 //var is the old way to declaring a variable
 var myNumber = 5;
 
@@ -16,3 +16,17 @@ myName = 'Ginger';
 //this is not : reassigning lastName is commented out so it does not throw an error
 const lastName = 'McCaleb';
 // lastName = 'Snap';
+
+// ~~~~~~~~~ ARROW FUNCTIONS ~~~~~~~~~~~
+
+//old way
+function printMyNameOldWay(name){
+    console.log(name);
+}
+printMyNameOldWay('Justin - OLD');
+
+//Arrow function - assign it to a const and use updated syntax
+const printMyNameNew = (name) => {
+    console.log(name);
+}
+printMyNameNew('Justin - NEW')
