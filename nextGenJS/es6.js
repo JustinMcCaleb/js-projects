1adb6c35c3bcd1545a5db1a5c514090876a80cec
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-21 16:13:18 -0500

    es6 let and const

---
+++ nextGenJS/es6.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//var is the old way to declaring a variable
+var myNumber = 5;
+
+//use let to declare a variable that will change in value
+let myOtherNumber = 5;
+
+//use const to declare something once and never change the value
+const PI = Math.PI;
