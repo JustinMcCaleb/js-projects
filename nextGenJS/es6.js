5db3b74d321f9de9c4d3d2d6cb9fec318611774f
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-21 16:17:11 -0500

    difference in let and const example

--- nextGenJS/es6.js
+++ nextGenJS/es6.js
@@ -8,3 +8,11 @@ let myOtherNumber = 5;
 
 //use const to declare something once and never change the value
 const PI = Math.PI;
+
+//this is valid
+let myName = 'Justin';
+myName = 'Ginger';
+
+//this is not : reassigning lastName is commented out so it does not throw an error
+const lastName = 'McCaleb';
+// lastName = 'Snap';
